56ec0f5970422d3b67e82dbffa607f643d7f2fa3
Vinayak Mundakkal <playstorepointsusa@gmail.com>
2022-01-31 12:20:39 +0530

    final

--- bot/helpers/imdb_api.py
+++ bot/helpers/imdb_api.py
@@ -5,10 +5,6 @@ imdb_query = "https://api.abirhasan.wtf/imdb/search?query="
 imdb_link = "https://api.abirhasan.wtf/imdb/get?link="
 
 async def get_info(query):
-    titles = []
-    msgs = []
-    photos = []
-
     data = requests.get(f"{imdb_query}{query}").json()
     for item in data.get("data"):
         try:
@@ -63,6 +59,33 @@ async def reqLinkID(query):
         id_list.append(None)
         titles.append(None)
     return titles, id_list
+
+async def get_info_from_id(link):
+    data = requests.get(imdb_link + link).json()
+
+    title = data.get("title")
+    story_line = str(data.get("story_line"))
+    director = str(data.get("directors"))
+    release_date = str(data.get("release"))
+    rating = str(data.get("rating"))
+
+    data2 = requests.get(f"{imdb_query}{title}").json()
+    for items in data2.get("data"):
+        check_link = items.get("imdb_link")
+        if check_link == link:
+            photo = items.get("thumbnail")
+            break
+    
+    msg = f"<b>Title :</b> <code>{title}</code>\n\n"
+    msg += f"<b>IMDB Rating :</b> <code>{rating}</code>\n\n"
+    msg += f"<b>Release :</b> <code>{release_date}</code>\n\n"
+    msg += f"<b>Director :</b> <code>{director}</code>\n\n"
+    msg += f"<b>Storyline :</b> <code>{story_line}</code>"
+
+    if FOOTER:
+        msg += f"\n\n{FOOTER}"
+
+    return msg, photo
 
 
 
--- bot/plugins/imdb.py
+++ bot/plugins/imdb.py
@@ -1,7 +1,9 @@
 from pyrogram import Client, filters
-from bot import BOT_USERNAME
-from bot.helpers.imdb_api import reqLinkID
+from bot import BOT_USERNAME, app
+from bot.helpers.imdb_api import reqLinkID, get_info_from_id
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+imdb_link_prefix = "https://www.imdb.com/title/"
 
 @Client.on_message(filters.command(["imdb", f"imdb@{BOT_USERNAME}"]))
 async def imdb(bot, update):
@@ -33,4 +35,39 @@ async def imdb(bot, update):
                 chat_id=update.chat.id,
                 text="Please enter the movie title 😇",
                 reply_to_message_id=update.message_id
+            )
+
+@app.on_callback_query()
+async def req_movie_cb(c: Client, cb: CallbackQuery):
+    if cb.data == "close":
+        await c.delete_messages(
+            chat_id=cb.message.chat.id,
+            message_ids=cb.message.message_id
+        )
+        try:
+            await c.delete_messages(
+                chat_id=cb.message.chat.id,
+                message_ids=cb.message.reply_to_message.message_id
+            )
+        except:
+            pass
+    else:
+        link = imdb_link_prefix + str(cb.data)
+        msg, photo = await get_info_from_id(link)
+        if msg:
+            await c.delete_messages(
+                chat_id=cb.message.chat.id,
+                message_ids=cb.message.message_id
+            )
+            await c.send_photo(
+                chat_id=cb.message.chat.id,
+                photo=photo,
+                caption=msg,
+                reply_to_message_id=cb.message.reply_to_message.message_id
+            )
+        else:
+            await c.edit_message_text(
+                chat_id=cb.message.chat.id,
+                message_id=cb.message.message_id,
+                text="Sorry, Couldnt Find A Proper Source For This Movie 😥"
             )
