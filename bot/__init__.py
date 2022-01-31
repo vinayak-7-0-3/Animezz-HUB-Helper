669a3f6f8e6109ef73e03d0a794f6b62466a78bb
Vinayak Mundakkal <playstorepointsusa@gmail.com>
2022-01-31 19:58:16 +0530

    fixes

--- bot/__init__.py
+++ bot/__init__.py
@@ -14,24 +14,15 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
-#STRING_SESSION = getenv("STRING_SESSION")
+
 BOT_USERNAME = getenv("BOT_USERNAME")
 if BOT_USERNAME.startswith("@"):
         BOT_USERNAME = BOT_USERNAME[1:]
 
-# Logging Group where RSS bot dumps
-#GROUP_ID = int(getenv("GROUP_ID"))
 CHANNEL_ID = int(getenv("CHANNEL_ID"))
+ADMINS = set(int(x) for x in getenv("ADMINS", "").split())
 
-# Both URLS (not the username)
-SUPPORT_GROUP = getenv("SUPPORT_GROUP")
 FOOTER = getenv("FOOTER")
-
-#MSG_TIMEOUT = int(getenv("MSG_TIMEOUT"))
-
-#DATABASE_URL = getenv("DATABASE_URL")
-#DATABASE_NAME = getenv("DATABASE_NAME")
-
 INLINE_THUMB = getenv("INLINE_THUMB", False)
 
 plugins = dict(
--- bot/plugins/post.py
+++ bot/plugins/post.py
@@ -1,21 +1,27 @@
+from bot import BOT_USERNAME, ADMINS, CHANNEL_ID
 from pyrogram import Client, filters
-from bot import BOT_USERNAME, CHANNEL_ID
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@Client.on_message(filters.command(["post", f"post@{BOT_USERNAME}"]))
-async def post_to_channel(bot, update):
-    try:
-        links = update.text.split("\n")
-    except:
-        links = None
-    msg = update.reply_to_message
-    if msg and links:
-        inline_keyboard = []
-        inline_keyboard.append([InlineKeyboardButton(text="Direct Download",url=links[1])])
-        inline_keyboard.append([InlineKeyboardButton(text="TG File",url=links[2])])
-        await bot.copy_message(
-            chat_id=CHANNEL_ID,
-            from_chat_id=msg.chat.id,
-            message_id=msg.message_id,
-            reply_markup=InlineKeyboardMarkup(inline_keyboard)
-        )
+@Client.on_message(filters.command(["buttons", f"buttons@{BOT_USERNAME}"]))
+async def post_in_channel(bot, update):
+    if update.from_user.id in ADMINS:
+        try:
+            links = update.text.split("\n")
+            links.pop(0)    
+        except:
+            links = None
+        msg = update.reply_to_message
+        if msg and links:
+            inline_keyboard = []
+            for link in links:
+                name = link.split(" | ")[0]
+                url = link.split(" | ")[1]
+                inline_keyboard.append([InlineKeyboardButton(name, url=url)])
+            await bot.copy_message(
+                chat_id=CHANNEL_ID,
+                from_chat_id=msg.chat.id,
+                message_id=msg.message_id,
+                reply_markup=InlineKeyboardMarkup(inline_keyboard)
+            )
+
+
