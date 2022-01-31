095eebd38c7ba8f110945cd7bdef48b64560f2da
Vinayak Mundakkal <76476634+vinayak-7-0-3@users.noreply.github.com>
2022-01-31 12:58:20 +0530

    Update post.py

--- bot/plugins/post.py
+++ bot/plugins/post.py
@@ -4,10 +4,18 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command(["post", f"post@{BOT_USERNAME}"]))
 async def post_to_channel(bot, update):
+    try:
+        links = update.text.split("\n")
+    except:
+        links = None
     msg = update.reply_to_message
-    if msg:
+    if msg and links:
+        inline_keyboard = []
+        inline_keyboard.append([InlineKeyboardButton(text="Direct Download",url=links[1])])
+        inline_keyboard.append([InlineKeyboardButton(text="TG File",url=links[2])])
         await bot.copy_message(
             chat_id=CHANNEL_ID,
             from_chat_id=msg.chat.id,
-            message_id=msg.message_id
+            message_id=msg.message_id,
+            reply_markup=InlineKeyboardMarkup(inline_keyboard)
         )
