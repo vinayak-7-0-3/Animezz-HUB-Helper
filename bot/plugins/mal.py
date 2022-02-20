48a9830668418e8f605191d3168f7bcd473fd11d
Vinayak Mundakkal <playstorepointsusa@gmail.com>
2022-02-20 19:58:56 +0530

    Update mal.py

--- bot/plugins/mal.py
+++ bot/plugins/mal.py
@@ -2,17 +2,19 @@
 from pyrogram import Client, filters
 from bot import BOT_USERNAME
 from bot.helpers.mal_api import get_anime_list
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Client.on_message(filters.command(["anime", f"anime@{BOT_USERNAME}"]))
 async def anime_mal(bot, update):
     try:
         name = update.text.split(" ", maxsplit=1)[1]
+        print(name)
     except:
         name = None
     if name:
         titles, id_list = get_anime_list(name)
         if titles:
+            print(titles)
             inline_keyboard = []
             for id in id_list:
                 name = titles[id_list.index(id)]
@@ -24,4 +26,21 @@ async def anime_mal(bot, update):
                     reply_markup=InlineKeyboardMarkup(inline_keyboard),
                     reply_to_message_id=update.message_id
                 )
+
+@Client.on_callback_query(filters.regex("^[0-9]+$"))
+async def anime_mal_cb(c: Client, cb: CallbackQuery):
+    if cb.data == "close":
+        await c.delete_messages(
+            chat_id=cb.message.chat.id,
+            message_ids=cb.message.message_id
+        )
+        try:
+            await c.delete_messages(
+                chat_id=cb.message.chat.id,
+                message_ids=cb.message.reply_to_message.message_id
+            )
+        except:
+            pass
+    else:
+         pass
 
