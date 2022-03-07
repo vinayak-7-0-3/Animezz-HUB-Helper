d17adce2bdc97cd3d4e3d200a1a523251a54aa71
Vinayak Mundakkal <playstorepointsusa@gmail.com>
2022-03-07 10:31:09 +0530

    oops

--- bot/plugins/basics.py
+++ bot/plugins/basics.py
@@ -24,39 +24,3 @@ async def close_menu(c: Client, cb: CallbackQuery):
         )
     except:
         pass
-
-@Client.on_message(filters.command(["check_anime", f"check_anime@{BOT_USERNAME}"]))
-async def check_anime(bot, update):
-    if update.reply_to_message:
-        init_msg = await bot.send_message(
-            chat_id=update.chat.id,
-            text="Downloading .......",
-            reply_to_message_id=update.message_id
-        )
-        file_path = await bot.download_media(
-            message=update.reply_to_message,
-            file_name=DOWNLOAD_DIR + "/"
-        )
-        await bot.edit_message_text(
-            chat_id=update.chat.id,
-            message_id=init_msg.message_id,
-            text="Scanning........Uwu........"
-        )
-        msg, video_link = await tracemoe_trace(file_path)
-        await bot.edit_message_text(
-            chat_id=update.chat.id,
-            message_id=init_msg.message_id,
-            text=msg
-        )
-        if video_link:
-            await bot.send_video(
-                chat_id=update.chat.id,
-                video=video_link,
-                reply_to_message_id=update.message_id
-            )
-    else:
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text="Reply to trace",
-            reply_to_message_id=update.message_id
-        )
